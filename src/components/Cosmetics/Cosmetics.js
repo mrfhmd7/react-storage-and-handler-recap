de410635fee78420a60de70e26036256b6765bce
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-04 23:19:16 +0600

    update Cosmetics file

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cosmetics = () => {
      return (
           <div>
-               
+               <h1>Welcome to my Cosmetics website.</h1>
           </div>
      );
 };
