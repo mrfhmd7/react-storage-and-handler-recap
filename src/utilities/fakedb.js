57427fded32b891a737d69808c474310cacd189f
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-05 21:32:00 +0600

    set local storage in professional way

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,16 +1,44 @@
 // use local storage to manage cart data
 
 const addToDb = id => {
-     const quantity = localStorage.getItem(id);
+     // this is a good way to practice local storage
+
+     let shoppingCart;
+
+     const storedCart = localStorage.getItem('shopping-cart');
+     if (storedCart) {
+          shoppingCart = JSON.parse(storedCart);
+     }
+     else {
+          shoppingCart = {};
+     }
+
+     const quantity = shoppingCart[id];
+
      if (quantity) {
-          // console.log('already exists', quantity);
-          const newQuantity = parseInt(quantity) + 1;
-          localStorage.setItem(id, newQuantity)
+          const newQuantity = quantity + 1;
+          shoppingCart[id] = newQuantity;
      }
-     else { 
-          // console.log('new item');
-          localStorage.setItem(id, 1);
+     else {
+          shoppingCart[id] = 1;
      }
+
+     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
+
+
+     // this is not a good way to practice local storage;
+
+     // const quantity = localStorage.getItem(id);
+     // if (quantity) {
+     //      // console.log('already exists', quantity);
+     //      const newQuantity = parseInt(quantity) + 1;
+     //      localStorage.setItem(id, newQuantity)
+     // }
+     // else { 
+     //      // console.log('new item');
+     //      localStorage.setItem(id, 1);
+     // }
 }
 
 export { addToDb };
