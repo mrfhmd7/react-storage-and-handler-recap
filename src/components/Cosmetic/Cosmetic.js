9fa76ec82365c54963004da00c2868790c664eaf
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-05 19:42:58 +0600

    set item in local storage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Cosmetic.css'
+import { addToDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
      // console.log(props.cosmetic);
      const { name, price ,id } = props.cosmetic;
 
      const addToCart = (id) => {
-          console.log('added',id);
+          addToDb(id);
      }
      return (
           <div className='product'>
                <h2>Buy this: {name} </h2>
                <p>Only for: ${price}</p>
-               <small>id: {id}</small> <br />
+               <p><small>id: {id}</small></p>
                <button onClick={() => addToCart(id)}>Add to cart</button>
           </div>
      );
